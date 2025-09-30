f9de997981b8091e71aea6a72215b660893c3b6f
Kim NoYoon <kimny706@hanmail.net>
2025-09-30 16:56:25 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+SELECT 
+    ugb.title, 
+    ugb.board_id, 
+    ugr.reply_id, 
+    ugr.writer_id, 
+    ugr.contents, 
+    DATE_FORMAT(ugr.created_date, '%Y-%m-%d')
+FROM (
+    SELECT board_id, title, contents
+    FROM USED_GOODS_BOARD
+    WHERE created_date LIKE '2022-10%'
+) ugb
+JOIN (
+    SELECT board_id, reply_id, writer_id, contents, created_date
+    FROM USED_GOODS_REPLY
+) ugr
+USING(board_id)
+ORDER BY ugr.created_date, ugb.title
